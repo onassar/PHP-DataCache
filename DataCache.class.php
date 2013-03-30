5e136dcaef03d7fbbf5a2bfcc6887aad5380e5c2
Oliver Nassar <onassar@gmail.com>
2013-03-29 22:11:00 -0400

    Cache engine ups

--- DataCache.class.php
+++ DataCache.class.php
@@ -10,13 +10,72 @@
     /**
      * DataCache
      * 
-     * @note     Should on be used with native data structures. Don't try
+     * @note     Should only be used with native data structures. Don't try
      *           caching stdClass', or something of that ilk.
      * @author   Oliver Nassar <onassar@gmail.com>
      * @abstract
      */
     abstract class DataCache
     {
+        /**
+         * _cacheType
+         * 
+         * @var    string
+         * @access protected
+         */
+        protected static $_cacheType;
+
+        /**
+         * _getPersistentCacheValue
+         * 
+         * @access protected
+         * @static
+         * @param  string $key
+         * @return mixed
+         */
+        protected static function _getPersistentCacheValue($key)
+        {
+            if (self::$_cacheType === 'memcached') {
+                return MemcachedCache::read($key);
+            } elseif (self::$_cacheType === 'apc') {
+                return APCCache::read($key);
+            }
+        }
+
+        /**
+         * _writeToPersistentCache
+         * 
+         * @access protected
+         * @static
+         * @param  string $key
+         * @param  mixed $value
+         * @return void
+         */
+        protected static function _writeToPersistentCache($key, $value)
+        {
+            if (self::$_cacheType === 'memcached') {
+                MemcachedCache::write($key, $value);
+            } elseif (self::$_cacheType === 'apc') {
+                APCCache::write($key, $value);
+            }
+        }
+
+        /**
+         * init
+         * 
+         * @access public
+         * @static
+         * @param  string $cacheType
+         * @return void
+         */
+        public static function init($cacheType)
+        {
+            if (!in_array($cacheType, array('apc', 'memcached'))) {
+                throw new Exception('Invalid cache type specified.');
+            }
+            self::$_cacheType = $cacheType;
+        }
+
         /**
          * read
          * 
@@ -29,11 +88,11 @@
         {
             $requestCacheValue = RequestCache::read($key);
             if ($requestCacheValue === null) {
-                $memcachedCacheValue = MemcachedCache::read($key);
-                if ($memcachedCacheValue === null) {
+                $persistentCacheValue = self::_getPersistentCacheValue($key);
+                if ($persistentCacheValue === null) {
                     return null;
                 }
-                return $memcachedCacheValue;
+                return $persistentCacheValue;
             }
             return $requestCacheValue;
         }
@@ -50,6 +109,6 @@
         public static function write($key, $value)
         {
             RequestCache::write($key, $value);
-            MemcachedCache::write($key, $value);
+            self::_writeToPersistentCache($key, $value);
         }
     }
